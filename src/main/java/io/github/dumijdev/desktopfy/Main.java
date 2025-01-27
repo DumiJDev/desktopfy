0df880b9ea88f7d558d7cccbe657a9fc694f9bc5
Dumildes Paulo <dumildesfernando@outlook.com>
2025-01-27 18:11:34 +0100

    chore: upgrade javafx-web version from 20 to 21

--- src/main/java/io/github/dumijdev/desktopfy/Main.java
+++ src/main/java/io/github/dumijdev/desktopfy/Main.java
@@ -1,17 +1,16 @@
 package io.github.dumijdev.desktopfy;
 
 import java.io.IOException;
+import java.time.Duration;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    Desktopfy.mainPage(Desktopfy.class.getResource("/index.html").getPath().replaceFirst("/", ""))
-        .width(800)
-        .height(600)
-        .resizable(true)
-        .open();
+  public static void main(String[] args) throws IOException, InterruptedException {
+    final var url = args.length > 0 ? args[0] : "https://github.com/dumijdev/dumijdev";
 
-    /*var desktop1 = Desktopfy.url("https://github.com/dumijdev")
-        .javaFxContainer()
-        .open();*/
+    final var desktop1 = Desktopfy.url(url).javaFxContainer().open();
+
+    Thread.sleep(Duration.ofSeconds(10).toMillis());
+
+    desktop1.close();
   }
 }
